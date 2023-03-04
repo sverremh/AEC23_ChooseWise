1a3dbeff02eba0b8a48c2cf7984c4cf7a4830bd5
Marcin Luczkowski <marcinl@ntnu.no>
2023-03-04 18:32:14 +0100

    Merge branch 'main' of https://github.com/sverremh/AEC23_ChooseWise

--- Back_end/speckle.py
+++ Back_end/speckle.py
@@ -1,2 +1,22 @@
+from specklepy.api import operations
+from specklepy.api.wrapper import StreamWrapper
+
 def get_data_from_speckle(stream_id, object_id):
-    print("hello")
+    #inputs
+    #element_id = "1b55273fd839ade53ee994f90f8a98ba"
+    #stream_id = "9e730f9975"
+    #code
+    
+    stream_url = "https://speckle.xyz/streams/" + stream_id
+    wrapper = StreamWrapper(stream_url)
+    transport = wrapper.get_transport()
+    rec = operations.receive(element_id, transport)
+    
+    msg_parts =[]
+    for wall in rec["@Walls"]:
+        info = "height wall = " + str(wall.height)
+        msg_parts.append(info)
+
+        
+    return msg_parts  
+    
