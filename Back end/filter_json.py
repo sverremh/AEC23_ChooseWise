8c6a6e1e016f1e9888706eef301d76c2926495ae
Sverre Magnus Haakonsen <sverre.m.haakonsen@ntnu.no>
2023-03-04 12:47:54 +0100

    test_json_el

--- Back end/filter_json.py
+++ Back end/filter_json.py
@@ -29,29 +29,50 @@ def filter_json_by_classification_text(json_lst, search_strings):
     
     return selected_elems
 
+def get_post_by_code(json_path, code_str):
+    """Get post from json by code string"""
+    with open(json_path, 'r', encoding='utf-8') as file:
+        data_list = json.load(file)
+    filtered_post = [el for el in data_list if el['number'] == code_str]
 
-# try with new methods
-df = pd.read_json('Data\\RandomPriceRenBD.json')
+    #TODO return information if nothing is found
+    return filtered_post
 
-with open('Data\\RandomPriceRenBD.json', mode="r", encoding="utf-8") as file:
-    json_list = json.load(file) # list of nested dictionaries. One for each roof post
-
-# Find unique chapters
-chapters = [] # list of the chapter for each post
-for post in json_list: # iterate through all posts in the json list
-    post_chapter = post['classifications'][5]['text']
-    chapters.append(post['classifications'][5]['text'])
-print(chapters)
-
-
-
-search_string = ['Væg', 'Vindu']
-filtered_json = filter_json_by_classification_text(json_list, search_strings = search_string)
+def write_json(list, path):
+    """Write list of dicts to json"""
+    with open(path, "w") as new_data:
+        json.dump(list, new_data)
 
 
-# write to json with just walls
-if isinstance(search_string, list):
-    search_string = ''.join(search_string)
-new_path = f'Data\\json_{search_string}.json'
-with open(new_path, "w") as new_data:
-   json.dump(filtered_json, new_data)
+
+
+
+
+if __name__ == '__main__':
+    search_post = '8.2-8.1,01'
+    print('Hello world')
+
+    # open the json
+    with open('Data\\RandomPriceRenBD.json', mode="r", encoding="utf-8") as file:
+        json_list = json.load(file) # list of nested dictionaries. One for each roof post
+
+    # Find unique chapters
+    chapters = [] # list of the chapter for each post
+    for post in json_list: # iterate through all posts in the json list
+        post_chapter = post['classifications'][5]['text']
+        chapters.append(post['classifications'][5]['text'])
+    print(chapters)
+
+
+    search_string = ['Væg', 'Vindu']
+    filtered_json = filter_json_by_classification_text(json_list, search_strings = search_string)
+    # change name to find string
+    if isinstance(search_string, list):
+        search_string = ''.join(search_string)
+
+    # find post in json
+    post_dict = get_post_by_code('Data\\json_VægVindu.json', search_post)
+      
+    # write to json with just walls    
+    new_path = f'Data\\json_{search_post}.json'
+    write_json(filtered_json, new_path)
