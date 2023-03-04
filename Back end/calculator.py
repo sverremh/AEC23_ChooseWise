eff30f2e4c0814c25e26c63dbf0a0ccca109f3ae
ArturTomczak <artomczak@gmail.com>
2023-03-04 11:32:27 +0100

    Create calculator.py

---
+++ Back end/calculator.py
@@ -0,0 +1,21 @@
+
+
+input_data = {"Roofs": [{"ID":1234, "Volume":2.5, "Materials":[{"Name":"Concrete", "Ratio":0.7}, {"Name":"Insulation", "Ratio":0.3}]}]}
+
+def calculate_cost(element):
+    return cost
+
+def calculate_lca(element):
+    return cost
+
+def calculate_time(element):
+    return cost
+
+def send_to_speckle(element, cost, lca, time):
+    pass
+
+for element in input_data:
+    cost = calculate_cost(element)
+    lca = calculate_lca(element)
+    time = calculate_time(element)
+    send_to_speckle(element, cost, lca, time)
