8aeec9824554f7cd9daa7c505a88b492ad01d40e
boniqa <budnik.wer@gmail.com>
2023-03-04 18:43:19 +0100

    wip

--- Back_end/speckle.py
+++ Back_end/speckle.py
@@ -3,14 +3,14 @@ from specklepy.api.wrapper import StreamWrapper
 
 def get_data_from_speckle(stream_id, object_id):
     #inputs
-    #element_id = "1b55273fd839ade53ee994f90f8a98ba"
+    #object_id = "1b55273fd839ade53ee994f90f8a98ba"
     #stream_id = "9e730f9975"
     #code
     
     stream_url = "https://speckle.xyz/streams/" + stream_id
     wrapper = StreamWrapper(stream_url)
     transport = wrapper.get_transport()
-    rec = operations.receive(element_id, transport)
+    rec = operations.receive(object_id, transport)
     
     msg_parts =[]
     for wall in rec["@Walls"]:
